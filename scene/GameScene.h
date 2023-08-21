18d7e3d836ade704d54315a26dc6cd062acbd421
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-08-22 01:33:43 +0900

    ユニークポインタ修正

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,7 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "Player.h"
+#include "../MyClass/GameObject/Player/Player.h"
 #include <memory>
 
 /// <summary>
